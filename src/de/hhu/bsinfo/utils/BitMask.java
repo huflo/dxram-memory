429dac555b13a47e5839b08936806783a2a5bcbd
Florian Hucke <florian.hucke@uni-duesseldorf.de>
2018-02-07 13:41:22 +0100

    BitMask.java: A new helper class to handle bit masks

---
+++ src/de/hhu/bsinfo/utils/BitMask.java
@@ -0,0 +1,52 @@
+package de.hhu.bsinfo.utils;
+
+/**
+ * A helper class to create bit masks
+ *
+ * @author Florian Hucke (florian.hucke@hhu.de) on 07.02.18
+ * @projectname dxram-memory
+ */
+public class BitMask {
+
+    final int bitSite;
+    long control = 0x0;
+
+    public BitMask(int byteSizeForBitMask){
+        bitSite = byteSizeForBitMask * 8;
+    }
+
+    /**
+     * Create a bit mask. Check if there are a union with previous created bit masks
+     * @param numberOfOnes number of ones
+     * @param offset offset of the lowest one
+     * @return a checked bit mask
+     */
+    public long checkedCreate(long numberOfOnes, int offset){
+
+        long ret = create(numberOfOnes, offset);
+
+        assert (control & ret) == 0 : "overlapping masks. control = " +
+                String.format("0x%016X", control) + " ret = " + String.format("0x%016X", ret);
+        control |= ret;
+
+        return ret;
+    }
+
+    /**
+     * create bit masks
+     * @param numberOfOnes number of ones
+     * @param offset offset from the LSB
+     * @return a bit mask
+     */
+    public long create(long numberOfOnes, int offset){
+        assert numberOfOnes+offset <= bitSite : "bit mask result in a overflow";
+
+        long ret = 0;
+        for (int i = 0; i < numberOfOnes; i++) {
+            ret = (ret << 1) | 1;
+        }
+
+        return ret << offset;
+    }
+
+}
