29df7b91d1ae865900563e2e4b1ee06994e163a4
Florian Hucke <florian.hucke@uni-duesseldorf.de>
2018-03-13 12:52:16 +0100

    MultiThreadMeasurementHelper: Remodeling use Value Percentile class

--- src/de/hhu/bsinfo/utils/eval/MultiThreadMeasurementHelper.java
+++ src/de/hhu/bsinfo/utils/eval/MultiThreadMeasurementHelper.java
@@ -1,5 +1,7 @@
 package de.hhu.bsinfo.utils.eval;
 
+import de.hhu.bsinfo.dxutils.stats.ValuePercentile;
+
 import java.io.IOException;
 import java.nio.file.Paths;
 import java.util.concurrent.atomic.AtomicLong;
@@ -11,11 +13,13 @@ import java.util.concurrent.atomic.AtomicLong;
  * @author Florian Hucke (florian.hucke@hhu.de) on 06.03.18
  * @projectname dxram-memory
  */
+@SuppressWarnings("SameParameterValue")
 public class MultiThreadMeasurementHelper {
 
     private final Measurement[] measurements;
     private final String folder;
     private final String descLine;
+    private static boolean collectMiss = false;
 
     private static long round = 0;
 
@@ -26,9 +30,24 @@ public class MultiThreadMeasurementHelper {
      * @param p_descLine Description of the measurement
      * @param p_measurements Names of the measurements
      */
-    public MultiThreadMeasurementHelper(String p_folder, String p_descLine, String... p_measurements) {
+    public MultiThreadMeasurementHelper(final String p_folder, final String p_descLine,
+                                        final String... p_measurements) {
+        this(p_folder, p_descLine, false, p_measurements);
+
+    }
+    /**
+     * Constructor
+     *
+     * @param p_folder Folder to save the measurement results
+     * @param p_descLine Description of the measurement
+     * @param p_measurements Names of the measurements
+     * @param p_collectMiss Collect data of failed operations
+     */
+    public MultiThreadMeasurementHelper(final String p_folder, final String p_descLine, final boolean p_collectMiss,
+                                        final String... p_measurements) {
         folder = (p_folder.endsWith("/")?p_folder:(p_folder + "/"));
         descLine = p_descLine;
+        collectMiss = p_collectMiss;
 
         measurements = new Measurement[p_measurements.length];
         for (int i = 0; i < p_measurements.length; i++) {
@@ -42,7 +61,7 @@ public class MultiThreadMeasurementHelper {
      * @param name Name of the measurement
      * @return The measurement instance of null if no suitable instance was found
      */
-    public Measurement getMeasurement(String name) {
+    public final Measurement getMeasurement(final String name) {
         for (Measurement m:measurements)
             if (m.name.equals(name))
                 return m;
@@ -52,47 +71,51 @@ public class MultiThreadMeasurementHelper {
 
     /**
      * Start a new round.
-     *
-     * @throws IOException The FileChannel can throw a IOException
      */
-    public void newRound() throws IOException {
+    public final void newRound() {
         round++;
-        for (Measurement m:measurements)
-            m.createFile();
     }
+
+
 
     /**
      * Write collected stats to a file
      *
      * @throws IOException The FileChannel can throw a IOException
      */
-    public void writeStats() throws IOException {
-        MultiThreadWriter mtw = new MultiThreadWriter(Paths.get(folder + "stats_" + round), descLine);
-        mtw.add(getStats());
+    public final void writeStats() throws IOException {
+        MultiThreadWriter mtw = new MultiThreadWriter(Paths.get(folder + "stats_" + round + ".csv"), descLine);
+        mtw.add(measurements[1].csvHeader(','));
+        mtw.add(getCSVStats(','));
         mtw.close();
     }
 
     /**
      * Get all stats of the measurements
      *
-     * @return A String of all measurements.
+     * @param delim The separator
+     * @return A string of all measurements
      */
-    public String getStats(){
+    private String getCSVStats(final char delim) {
         StringBuilder out = new StringBuilder();
+
         for (Measurement m:measurements)
-            out.append(m);
+            out.append(m.getExecutedSuccessfullyStats(delim));
 
         return out.toString();
     }
+
 
     /**
      * Single measurement
      *
      * @author Florian Hucke (florian.hucke@hhu.de) on 06.03.18
      */
-    public class Measurement{
-        private String name;
-        private MultiThreadWriter m_multiThreadWriter;
+    public final class Measurement{
+        private final String name;
+
+        private final ValuePercentile percentileHit;
+        private final ValuePercentile percentileMiss;
 
         private final AtomicLong hit = new AtomicLong(0);
         private final AtomicLong hit_accu_time = new AtomicLong(0);
@@ -111,45 +134,36 @@ public class MultiThreadMeasurementHelper {
          */
         Measurement(final String p_name) {
             name = p_name;
-            try {
-                createFile();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+            percentileHit = new ValuePercentile(MultiThreadMeasurementHelper.Measurement.class, name);
+            percentileMiss = new ValuePercentile(MultiThreadMeasurementHelper.Measurement.class, name);
         }
 
         /**
          * Add a measured Time.
          *
          * @param ok check if a hit or a miss occurred
-         * @param startTime the start time of the operation
+         * @param deltaTime Time delta
          */
-        public void addTime(final boolean ok, final long startTime) {
-            long time = System.nanoTime()-startTime;
+        public final void addTime(final boolean ok, final long deltaTime) {
             if(ok){
                 hit.incrementAndGet();
-                hit_accu_time.getAndAdd(time);
-                hit_best.accumulateAndGet(time, Math::min);
-                hit_worst.accumulateAndGet(time, Math::max);
-            } else {
+                hit_accu_time.getAndAdd(deltaTime);
+                hit_best.accumulateAndGet(deltaTime, Math::min);
+                hit_worst.accumulateAndGet(deltaTime, Math::max);
+
+                synchronized (percentileHit){
+                    percentileHit.record(deltaTime);
+                }
+            } else if (collectMiss){
                 miss.incrementAndGet();
-                miss_accu_time.getAndAdd(time);
-                miss_best.accumulateAndGet(time, Math::min);
-                miss_worst.accumulateAndGet(time, Math::max);
+                miss_accu_time.getAndAdd(deltaTime);
+                miss_best.accumulateAndGet(deltaTime, Math::min);
+                miss_worst.accumulateAndGet(deltaTime, Math::max);
+
+                synchronized (percentileMiss) {
+                    percentileMiss.record(deltaTime);
+                }
             }
-            m_multiThreadWriter.add(time);
-        }
-
-        /**
-         * Create a file and close the previous one
-         *
-         * @throws IOException The FileChannel can throw a IOException
-         */
-        private void createFile() throws IOException {
-            if(m_multiThreadWriter != null)
-                m_multiThreadWriter.close();
-
-            m_multiThreadWriter = new MultiThreadWriter(Paths.get(folder + name + "_" + round), descLine);
         }
 
         @Override
@@ -163,19 +177,70 @@ public class MultiThreadMeasurementHelper {
          *
          * @return A String
          */
-        String getStats(){
-            StringBuilder out = new StringBuilder(name).append('\n');
-            if(hit.get() > 0)
-                out.append(String.format("hit(%d)\t-> best: %d, worst: %d, avg: %d\n",
-                    hit.get(), hit_best.get(), hit_worst.get(), hit_accu_time.get()/hit.get()));
+        final String getStats(){
+            char delim = ',';
+            return getExecutedSuccessfullyStats(delim) +
+                    ((collectMiss)?getNotExecutedSuccessfullyStats(delim):"");
 
-            if(miss.get() > 0)
-                out.append(String.format("miss(%d)\t-> best: %d, worst: %d, avg: %d\n",
-                        miss.get(), miss_best.get(), miss_worst.get(), miss_accu_time.get()/miss.get()));
+        }
 
-            out.append("\n");
+        /**
+         * get the statistics of successfully completed tests in CSV format
+         *
+         * @param delim The separator
+         * @return The stats in CSV format
+         */
+        final String getExecutedSuccessfullyStats(final char delim) {
+            StringBuilder out = new StringBuilder();
+            if(hit.get() > 0) {
+                out.append(name).append(delim).append(hit.get()).append(delim)
+                        .append(hit_best.get()).append(delim).append(hit_worst.get())
+                        .append(delim);
+
+                if (hit.get() > 0)
+                    out.append(hit_accu_time.get() / hit.get());
+                else
+                    out.append(0);
+
+                out.append(delim).append(percentileHit.toCSV(delim)).append('\n');
+            }
 
             return out.toString();
+        }
+
+        /**
+         * get the statistics of successfully completed tests in CSV format
+         *
+         * @param delim The separator
+         * @return The stats in CSV format
+         */
+        final String getNotExecutedSuccessfullyStats(final char delim) {
+            StringBuilder out = new StringBuilder();
+            if(miss.get() > 0) {
+                out.append(name).append("(miss)").append(delim).append(miss.get()).append(delim)
+                        .append(miss_best.get()).append(delim).append(miss_worst.get())
+                        .append(delim);
+
+                if (miss.get() > 0)
+                    out.append(miss_accu_time.get() / miss.get());
+                else
+                    out.append(0);
+
+                out.append(delim).append(percentileMiss.toCSV(delim)).append('\n');
+            }
+
+            return out.toString();
+        }
+
+        /**
+         * Create a CSV header.
+         *
+         * @param delim The separator
+         * @return a header for the CSV file
+         */
+        private String csvHeader(final char delim){
+            return "name" + delim + "operation" + delim + "best" + delim + "worst" + delim + "average"
+                    + delim + percentileHit.generateCSVHeader(delim) + '\n';
         }
     }
 }
