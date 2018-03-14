5aa6df8405e5de9a2054ee495b1fe58b1fd6b5fc
Florian Hucke <florian.hucke@uni-duesseldorf.de>
2018-03-14 12:09:10 +0100

    start-dxram-memory.sh: Change start script

--- start-dxram-memory.sh
+++ start-dxram-memory.sh
@@ -3,7 +3,7 @@
 ## Script information
 # author: Florian Hucke, florian(dot)hucke(at)hhu(dot)de
 # date: 02. Feb 2018
-# version: 0.1
+# version: 0.5
 # license: GPL V3
 
 LIB_DIR=$PWD/lib
@@ -11,21 +11,42 @@ BIN_DIR=$PWD/bin
 CONF_DIR=$PWD/config
 
 
-BINARY=$BIN_DIR/dxram-memory-develop.jar
-if [ ! -e $BINARY ]; then
+BINARY=${BIN_DIR}/dxram-memory-develop.jar
+
+if [ ! -e ${BINARY} ]; then
     echo "No jar found at $BINARY"
-    echo "run the script: \"build_dxram_memory\""
-    exit 1
+    echo "Build DXRAM-Memory"
+    $PWD/build-dxram-memory.sh
+    BINARY=${BIN_DIR}/dxram-memory-develop.jar
 fi
 
-LIBS=$LIB_DIR/slf4j-api-1.6.1.jar:$LIB_DIR/gson-2.7.jar:$LIB_DIR/log4j-api-2.7.jar:$LIB_DIR/log4j-core-2.7.jar:$LIB_DIR/jline-2.15.jar
+create_lib_path() {
+    TMP=""
+    for i in $@; do
+        if [ -z ${TMP} ]; then
+            TMP=${LIB_DIR}/${i}
+        else
+            TMP=${TMP}:${LIB_DIR}/${i}
+        fi
+    done
 
-CLASSPATH=$LIBS:$BINARY
+    echo ${TMP}
+}
+
+LIBS=$(create_lib_path \
+            slf4j-api-1.6.1.jar \
+            gson-2.7.jar \
+            log4j-api-2.7.jar \
+            log4j-core-2.7.jar \
+            jline-2.15.jar \
+            perf-timer.jar)
+
+CLASSPATH=${LIBS}:${BINARY}
 
 MAIN_CLASS=de.hhu.bsinfo.DXRAMMemory
 
 java -ea\
-    -Dlog4j.configurationFile=$CONF_DIR/log4j.xml\
-    -Ddxram.config=$CONF_DIR/dxram.json\
-    -cp $CLASSPATH $MAIN_CLASS\
+    -Dlog4j.configurationFile=${CONF_DIR}/log4j.xml\
+    -Ddxram.config=${CONF_DIR}/dxram.json\
+    -cp ${CLASSPATH} ${MAIN_CLASS}\
     $@ #> /dev/null 2>&1
